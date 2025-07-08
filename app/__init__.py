7dc5557cc168e0ba0ac807e5c3bcfdba0c8ee63e
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-07-08 01:05:35 -0500

    nuevo push

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,10 @@ from app.routes.auth import auth_bp
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, origins=["http://localhost:8080"], supports_credentials=True)
+    CORS(app, origins=[
+        "http://localhost:8080",  
+        "https://assistant-virtual-production.onrender.com"  
+    ], supports_credentials=True)
 
     app.config["JWT_SECRET_KEY"] = "b7f$2K!9zQw@1xR8pLeT6vN3sJ0uY5c"  
     jwt = JWTManager(app)
