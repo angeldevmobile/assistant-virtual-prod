871698d02151294e920a3320298d4c0dac6c7023
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-07-08 01:27:30 -0500

    nuevo cambio ruta

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,15 @@ from app.routes.auth import auth_bp
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, origins=[
-        "http://localhost:8080",  
-        "https://assistant-virtual-production.onrender.com"  
-    ], supports_credentials=True)
-
-    app.config["JWT_SECRET_KEY"] = "b7f$2K!9zQw@1xR8pLeT6vN3sJ0uY5c"  
+    CORS(
+        app,
+        origins=[
+            "http://localhost:8080",
+            "https://assistant-gem-virtual.onrender.com",  # frontend en producción
+        ],
+        supports_credentials=True,
+    )
+    app.config["JWT_SECRET_KEY"] = "b7f$2K!9zQw@1xR8pLeT6vN3sJ0uY5c"
     jwt = JWTManager(app)
 
     app.register_blueprint(auth_bp, url_prefix="/api/v1")
