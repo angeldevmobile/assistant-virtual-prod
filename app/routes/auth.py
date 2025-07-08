4014f145c78aa192f387b50034daba219ed47dfa
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-07-08 03:33:59 -0500

    ajustando emojis

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -589,9 +589,17 @@ def remove_emojis(text):
         "\U0001F600-\U0001F64F"  # emoticons
         "\U0001F300-\U0001F5FF"  # symbols & pictographs
         "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\U0001F1E0-\U0001F1FF"  # flags
         "\U00002700-\U000027BF"  # Dingbats
         "\U000024C2-\U0001F251"
+        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+        "\U00002600-\U000026FF"  # Misc symbols
+        "\U0001F700-\U0001F77F"  # Alchemical Symbols
+        "\U0000200D"             # Zero Width Joiner
+        "\U00002300-\U000023FF"  # Misc Technical
+        "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+        "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
         "]+",
         flags=re.UNICODE,
     )
